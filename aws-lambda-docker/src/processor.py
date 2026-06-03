34b178777eee76b2aa6c4372486a7b80a0881bb7
Michael Hawkins <mjh39@cam.ac.uk>
2026-06-03 10:15:11 +0100

    refactor(processor): source SEARCH_HOST from config like other search vars

--- aws-lambda-docker/src/processor.py
+++ aws-lambda-docker/src/processor.py
@@ -124,6 +124,7 @@ def run_ant(config: Config, tei_file: str, *, stream_stdout: bool = False) -> No
     env = {
         **os.environ,
         "ENVIRONMENT": "local",
+        "SEARCH_HOST": config.search_host,
         "SEARCH_COLLECTION_PATH": config.search_collection_path,
         "SEARCH_PORT": config.search_port,
         "SKIP_COPY_TEI_WEB_ASSETS": config.skip_copy_tei_web_assets,
